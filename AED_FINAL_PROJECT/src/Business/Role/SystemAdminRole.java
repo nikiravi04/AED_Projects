7b46a8489f1a2a56ce6f6dd2ef906fb884636e62
Srikanth Reddy Gubballi <gubballi.s@husky.neu.edu>
2018-11-21 11:35:58 -0500

    Packages and JPanlels for Different roles

--- AED_FINAL_PROJECT/src/Business/Role/SystemAdminRole.java
+++ AED_FINAL_PROJECT/src/Business/Role/SystemAdminRole.java
@@ -8,7 +8,7 @@ import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
-import userinterface.SystemAdminWorkArea.SystemAdminWorkAreaJPanel;
+import UserInterface.SystemAdminWorkArea.SystemAdminWorkAreaJPanel;
 import javax.swing.JPanel;
 
 /**
