72618800a105da754d63f9cb97da2995aec9f021
Srikanth Reddy Gubballi <gubballi.s@husky.neu.edu>
2018-11-26 16:13:29 -0500

    remove errors

--- AED_FINAL_PROJECT/src/UserInterface/SystemAdminWorkArea/ManageNetworkJPanel.java
+++ AED_FINAL_PROJECT/src/UserInterface/SystemAdminWorkArea/ManageNetworkJPanel.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package userinterface.SystemAdminWorkArea;
+package UserInterface.SystemAdminWorkArea;
 
 import Business.EcoSystem;
 import Business.Network.Network;
